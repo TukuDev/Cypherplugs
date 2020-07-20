4683732af1884ac08155558a9afc886b6076131b
domkinger <tehdjdrama@gmail.com>
2020-07-19 22:37:56 -0400

    added setup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "98.css";
+import Setup from "./Setup.js";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   return (
+      <div className="app">
+         <Setup />
+      </div>
+   );
 }
 
 export default App;
---
+++ frontend/src/Setup.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "98.css";
+import './Setup.css';
+
+const Setup = () => {
+
+   return (
+      <div className="window">
+         <div className="title-bar">
+            <div className="title-bar-text">Setup your Cypherplug</div>
+            <div className="title-bar-controls">
+               <button aria-label="Minimize" />
+               <button aria-label="Maximize" />
+               <button aria-label="Close" />
+            </div>
+         </div>
+         <div className="window-body" style={{ width: 320, height: 80 }} >
+            <label htmlFor="name">Give your Cyperhplug a name:</label>
+            <div className="field-row" style={{justifyContent: "center"}}>
+               <input id="name" type="text" className="name-input"></input>
+            </div>
+            <button className="next-button" onClick={() => console.log("next")}>Next</button>
+         </div>
+      </div>
+   );
+}
+
+export default Setup;
